02cddf986934573e73161d78d24daf50741938ba
Maria Agustina Santucho <asantucho35@gmail.com>
2023-02-06 18:07:13 -0300

    modifique el componente pasandole props haciendolo reutilizable para el resto de los testimonios a clonar

--- src/components/Testimony.jsx
+++ src/components/Testimony.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import "../styles/styles.css";
 
-function Testimony() {
+function Testimony(props) {
   return (
     <div className = "testimony-container">
       <img className = "image-testimony" 
-      src = {require("../images/testimonio-emma.png")}
-      alt = "imagen de Emma"/>
+      src = {require(`../images/testimonio-${props.image}.png`)}
+      alt = "Foto de testimonio"/>
       <div className = "testimony-text-container">
-      <p className='name-testimony'>Emma Bostian en Suecia</p>
-      <p className='job-testimony'>Ingeniero de Software en Sportify</p>
-      <p className='text-testimony'>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+      <p className='name-testimony'><strong>{props.name}</strong> en {props.country}</p>
+      <p className='job-testimony'>{props.job} en <strong>{props.company}</strong></p>
+      <p className='text-testimony'>"{props.testimony}"</p>
       </div>
     </div>
   )
